e36b1c92209f106b088031866ecbf13f4d70120a
xiandhcc/chenhongfei <chrunlee@foxmail.com>
2018-11-21 11:17:30 +0800

    error

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ query({
 var count = 0;
 var p2pspider = require('p2pspider');
 p2pspider(function(data){
-    console.log(data); //获取到的信息
     //将获得的信息存储在数据库中，以待后续在数据库中查询
    	//1.只保留
+   	data.pieces =null;
    	var name = data.name,
    		size = data.size,
    		address = data.address,
@@ -19,12 +19,14 @@ p2pspider(function(data){
    		infohash = data.infohash,
    		magnet = data.magnet,
    		str = JSON.stringify(data);
-
+   	console.log(name);
    	query({
    		sql : 'insert into demo_magnet (name,filesize,fileaddress,fileport,infohash,magnet,json) values (?,?,?,?,?,?,?)',
    		params : [name,size,address,port,infohash,magnet,str]
    	}).then(rs=>{
    		count++;
    		console.log(`共计获取信息:${count}条`);
+   	}).catch(err=>{
+   		console.log(err);
    	})
 });
