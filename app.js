656aa5025ebe838399a22b29ac9a0f643298635d
xiandhcc/chenhongfei <chrunlee@foxmail.com>
2018-11-21 11:09:29 +0800

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+var query = require('simple-mysql-query');
+query({
+	host : 'localhost',
+	port : '3306',
+	username : 'root',
+	password : 'root',
+	database : 'items'
+});
+var count = 0;
+var p2pspider = require('p2pspider');
+p2pspider(function(data){
+    console.log(data); //获取到的信息
+    //将获得的信息存储在数据库中，以待后续在数据库中查询
+   	//1.只保留
+   	var name = data.name,
+   		size = data.size,
+   		address = data.address,
+   		port = data.port,
+   		infohash = data.infohash,
+   		magnet = data.magnet,
+   		str = JSON.stringify(data);
+
+   	query({
+   		sql : 'insert into demo_magnet (name,filesize,fileaddress,fileport,infohash,magnet,json) values (?,?,?,?,?,?,?)',
+   		params : [name,size,address,port,infohash,magnet,str]
+   	}).then(rs=>{
+   		count++;
+   		console.log(`共计获取信息:${count}条`);
+   	})
+});
